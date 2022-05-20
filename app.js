4ad777d0daa8d6d9fb5225f93c783fa81fa5cc26
dhruv50ae <linkdp2012@gmail.com>
2022-05-20 14:38:16 +0100

    Created the basics of a todo list

--- app.js
+++ app.js
@@ -1,39 +1,29 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+
 const app = express();
+
 app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+let items = ["Buy food", "Cook food", "Eat food"];
 
 app.get("/", (req, res) => {
   let today = new Date();
-  let currentDay = today.getDate();
-  let day = "";
+  let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
+  let day = today.toLocaleDateString("en-US", options);
+  res.render("list", { kindOfDay: day, newListItems: items });
+});
 
-  switch (currentDay) {
-    case 0:
-      day = "Sunday";
-      break;
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-      day = "Tuesday";
-      break;
-    case 3:
-      day = "Wednesday";
-      break;
-    case 4:
-      day = "Thurday";
-      break;
-    case 5:
-      day = "Friday";
-      break;
-    case 6:
-      day = "Saturday";
-      break;
-    default:
-      console.log("Error: current day is equal to: " + currentDay);
-  }
-  res.render("list", { kindOfDay: day });
+app.post("/", (req, res) => {
+  let item = req.body.newItem;
+  items.push(item);
+  res.redirect("/");
 });
 
 app.listen(3000, () => {
